82b1980e5bb7cd934b783cf3eda737bcb368e6ee
AaricChen <chopinchen@vip.sina.com>
2016-07-30 21:23:11 +0800

    增加前端css和js构建

--- joshua-web/src/main/websrc/gulpfile.js
+++ joshua-web/src/main/websrc/gulpfile.js
@@ -2,15 +2,40 @@
  * Created by Administrator on 2016/7/30.
  */
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
+var minify = require('gulp-minify');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+    gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
 
 gulp.task('minify-css', function () {
     return gulp.src('css/*.css')
         .pipe(concatCss("joshua.min.css"))
-        .pipe(cleanCSS({debug: true}, function(details) {
+        .pipe(cleanCSS({debug: true}, function (details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
         .pipe(gulp.dest('../resources/public/assets/css/'));
+});
+
+gulp.task("minify-js", function () {
+    return gulp.src("js/*.js")
+        .pipe(concat("joshua.js"))
+        .pipe(minify({
+            ext: {
+                min: ".min.js"
+            },
+            noSource: true
+        }))
+        .pipe(gulp.dest('../resources/public/assets/js/'))
+});
+
+gulp.task("default", function () {
+    gulp.run("lint", "minify-js", "minify-css");
 });
