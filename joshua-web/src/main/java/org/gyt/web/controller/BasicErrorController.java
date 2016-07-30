419374d8cb0bea1396509af201c9e6d29e107e48
AaricChen <chopinchen@vip.sina.com>
2016-07-30 16:46:47 +0800

    增加404页面和错误处理逻辑

---
+++ joshua-web/src/main/java/org/gyt/web/controller/BasicErrorController.java
@@ -0,0 +1,43 @@
+package org.gyt.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * 一级页面路由器
+ * Created by y27chen on 2016/7/12.
+ */
+@RestController
+@RequestMapping("/error")
+public class BasicErrorController implements ErrorController {
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @RequestMapping
+    public ModelAndView handleError(HttpServletRequest request) {
+        Map<String, Object> errors = errorAttributes.getErrorAttributes(new ServletRequestAttributes(request), true);
+        ModelAndView modelAndView = new ModelAndView("404");
+
+        if (errors.get("status").equals(404)) {
+            modelAndView.setViewName("404");
+            modelAndView.addObject("timestamp", errors.get("timestamp"));
+            modelAndView.addObject("path", errors.get("path"));
+        }
+
+        return modelAndView;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
