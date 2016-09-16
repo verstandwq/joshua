ee8610ea1775d4e42570f32776c47cedcac6d1b2
Aaric <chopinchen@vip.sina.com>
2016-09-17 01:33:26 +0800

    添加后台首页

--- joshua-web/src/main/resources/websrc/js/nav.js
+++ joshua-web/src/main/resources/websrc/js/nav.js
@@ -2,6 +2,14 @@ $(document).ready(function () {
     $('.ui.dropdown').dropdown({on: 'hover'});
 });
 
+/**
+ * 后台导航栏所需
+ */
+$(document).ready(function () {
+    $('.admin .ui.accordion').accordion('refresh');
+    $('.admin .ui.dropdown').dropdown();
+});
+
 
 
 
