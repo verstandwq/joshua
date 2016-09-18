3ebc70253d27d15f2afd418eac586c8d442973e0
AaricChen <chopinchen@vip.sina.com>
2016-09-18 11:28:22 +0800

    静态页面调整

--- joshua-web/src/main/java/org/gyt/web/controller/StaticPageController.java
+++ joshua-web/src/main/java/org/gyt/web/controller/StaticPageController.java
@@ -44,81 +44,81 @@ public class StaticPageController {
         return new ModelAndView("staticPage/contactPage");
     }
 
+    /* 团契介绍 */
+    @RequestMapping("/fellowship")
+    public ModelAndView groupPage() {
+        return new ModelAndView("staticPage/fellowshipPage");
+    }
+
     /* 财务报告 */
     @RequestMapping("/financial")
     public ModelAndView financialPage() {
         return new ModelAndView("staticPage/financialPage");
     }
 
-    /* 团契介绍 */
-    @RequestMapping("/group")
-    public ModelAndView groupPage() {
-        return new ModelAndView("groupPage");
-    }
-
     /* 教堂简介 */
     @RequestMapping("/about")
     public ModelAndView introductionPage() {
-        return new ModelAndView("introductionPage");
+        return new ModelAndView("staticPage/introductionPage");
     }
 
     /* 教堂婚礼 */
     @RequestMapping("/wedding")
     public ModelAndView marriagePage() {
-        return new ModelAndView("marriagePage");
+        return new ModelAndView("staticPage/marriagePage");
     }
 
     /* 媒体资源 */
     @RequestMapping("/media")
     public ModelAndView mediaPage() {
-        return new ModelAndView("mediaPage");
+        return new ModelAndView("staticPage/mediaPage");
     }
 
     /* 新人 */
     @RequestMapping("/newcomer")
     public ModelAndView newComerPage() {
-        return new ModelAndView("newComerPage");
+        return new ModelAndView("staticPage/newComerPage");
     }
 
     /* 主任牧师 */
     @RequestMapping("/pastor")
     public ModelAndView pastorPage() {
-        return new ModelAndView("pastorPage");
-    }
-
-    /* 每周讲道 */
-    @RequestMapping("/preach")
-    public ModelAndView preachPage() {
-        return new ModelAndView("preachPage");
+        return new ModelAndView("staticPage/pastorPage");
     }
 
     /* 好文推荐 */
     @RequestMapping("/recommend")
     public ModelAndView recommendPage() {
-        return new ModelAndView("recommendPage");
+        return new ModelAndView("staticPage/recommendPage");
     }
 
     /* 事工报告 */
     @RequestMapping("/report")
     public ModelAndView reportPage() {
-        return new ModelAndView("reportPage");
+        return new ModelAndView("staticPage/reportPage");
+    }
+
+    /* 主内服侍 */
+    @RequestMapping("/service")
+    public ModelAndView servicePage() {
+        return new ModelAndView("staticPage/servicePage");
     }
 
     /* 儿童主日学 */
     @RequestMapping("/sunday")
     public ModelAndView sundaySchoolPage() {
-        return new ModelAndView("sundaySchoolPage");
+        return new ModelAndView("staticPage/sundaySchoolPage");
     }
 
     /* 见证奉献 */
     @RequestMapping("/testimony")
     public ModelAndView testimonyPage() {
-        return new ModelAndView("testimonyPage");
+        return new ModelAndView("staticPage/testimonyPage");
     }
 
     /* 主日崇拜 */
     @RequestMapping("/worship")
     public ModelAndView worshipPage() {
-        return new ModelAndView("worshipPage");
+        return new ModelAndView("staticPage/worshipPage");
     }
 }
