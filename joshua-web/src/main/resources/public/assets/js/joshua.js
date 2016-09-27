248e36710a44f1a07e50952c8975b17f7611b42e
AaricChen <chopinchen@vip.sina.com>
2016-09-27 17:15:53 +0800

    添加后台导航栏自动调整功能

--- joshua-web/src/main/resources/public/assets/js/joshua.js
+++ joshua-web/src/main/resources/public/assets/js/joshua.js
@@ -50595,7 +50595,29 @@ $(document).ready(function () {
  * 后台导航栏所需
  */
 $(document).ready(function () {
-    $('.ui.admin.menu .ui.accordion').accordion('refresh');
+    var getAccordionIndex = function () {
+        var location = window.location.pathname;
+
+        if (location.startsWith("/admin/static")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".static.item"));
+        } else if (location.startsWith("/admin/user/fellowship") || location.startsWith("/admin/fellowship")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".fellowship.item"));
+        } else if (location.startsWith("/admin/navigation")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".navigation.item"));
+        } else if (location.startsWith("/admin/notification")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".notification.item"));
+        } else if (location.startsWith("/admin/message")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".message.item"));
+        } else if (location.startsWith("/admin/user")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".user.item"));
+        } else if (location.startsWith("/admin/article")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".article.item"));
+        } else if (location.startsWith("/admin/info") || location.startsWith("/admin/password")) {
+            return $(".ui.admin.menu .ui.accordion > .item").index($(".info.item"));
+        }
+    };
+
+    $('.ui.admin.menu .ui.accordion').accordion('open', getAccordionIndex() - 1);
     $('.ui.admin.menu .ui.dropdown').dropdown();
 });
 
