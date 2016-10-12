5cb2067ccc71d1aa9d6def6b702768845dd2d64a
Aaric <chopinchen@vip.sina.com>
2016-10-12 21:57:14 +0800

    JS 修复

--- joshua-web/src/main/resources/websrc/js/lib-joshua-home.js
+++ joshua-web/src/main/resources/websrc/js/lib-joshua-home.js
@@ -2,14 +2,16 @@
  首页脚本组件
  */
 $(document).ready(function () {
-    var slider = new IdealImageSlider.Slider({
-        selector: '#slider',
-        height: 450,
-        interval: 4000,
-        effect: 'fade'
-    });
-    slider.addBulletNav();
-    slider.start();
+    if (document.getElementById("slider")) {
+        var slider = new IdealImageSlider.Slider({
+            selector: '#slider',
+            height: 450,
+            interval: 4000,
+            effect: 'fade'
+        });
+        slider.addBulletNav();
+        slider.start();
+    }
 });
 
 
