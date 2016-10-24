02b042faea768a19fe791610bb089da34fdf6deb
Aaric <chopinchen@vip.sina.com>
2016-10-24 13:28:26 +0800

    fix #160 添加后台日志

--- joshua-web/src/main/java/org/gyt/web/controller/config/WebConfiguration.java
+++ joshua-web/src/main/java/org/gyt/web/controller/config/WebConfiguration.java
@@ -2,6 +2,7 @@ package org.gyt.web.controller.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
