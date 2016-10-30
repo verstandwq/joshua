7cddc275c92cca1282facf654f2bd341f17bb093
AaricChen <chopinchen@vip.sina.com>
2016-10-30 21:01:15 +0800

    admin navigation done

--- joshua-web/src/main/resources/websrc/app/component/admin-navigation.js
+++ joshua-web/src/main/resources/websrc/app/component/admin-navigation.js
@@ -3,11 +3,13 @@
  * Created by y27chen on 2016/10/29.
  */
 import "../common";
+import Dialog from "./dialog";
 
 export default class Navigation {
     constructor() {
         $('.ui.admin.menu .ui.dropdown').dropdown();
         $('.ui.admin.menu .ui.accordion').accordion('open', this.getNavigationIndex() - 1);
+        $('.user.logout.item').on("click", this.logout);
     }
 
     getNavigationIndex() {
@@ -30,5 +32,26 @@ export default class Navigation {
         } else if (location.startsWith("/admin/info") || location.startsWith("/admin/password")) {
             return $(".ui.admin.menu .ui.accordion > .item").index($(".info.item"));
         }
+    }
+
+    logout() {
+        new Dialog("注销", "您确定要注销吗？", function () {
+            var formData = new FormData();
+            formData.append("_csrf", $(".user.logout.item form input[name='_csrf']").val());
+
+            $.ajax({
+                url: "/logout",
+                type: "post",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function () {
+                    window.location = '/login?logout';
+                },
+                error: function () {
+                    new Dialog("注销", "注销失败").error();
+                }
+            });
+        }).confirm();
     }
 }
